4d0b00be671e94846f19b39af3c18ca313b0a151
Kamila <abdurahmanovakamila.2006@gmail.com>
2025-03-05 11:44:57 +0500

    authors completed

--- authors/urls.py
+++ authors/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import AuthorListCreateView, AuthorDetailView
-
+from .views import AuthorListCreateView, AuthorDetailView, AuthorBooksView
 
 app_name = 'authors'
 
 urlpatterns = [
     path('', AuthorListCreateView.as_view(), name='authors-list'),
     path('<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
+    path('<int:pk>/books/', AuthorBooksView.as_view(), name='author-books'),
 ]
--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,11 @@
 from rest_framework import generics
+from rest_framework.status import HTTP_200_OK
+from rest_framework.views import APIView
 from .models import Author
 from .serializers import AuthorSerializer
+from django.shortcuts import get_object_or_404
+from books.serializers import BookSerializer
+from rest_framework.response import Response
 
 
 class AuthorListCreateView(generics.ListCreateAPIView):
@@ -10,3 +15,10 @@ class AuthorListCreateView(generics.ListCreateAPIView):
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+class AuthorBooksView(APIView):
+    def get(self, request, pk):
+        author = get_object_or_404(Author, pk=pk)
+        books = author.books.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
